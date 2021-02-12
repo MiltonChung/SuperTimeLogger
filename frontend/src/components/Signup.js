a82bb5347695b66f0f39fa9b42866b85bc28c62f
Milton Chung <miltonjchung@gmail.com>
2021-02-11 23:27:20 -0800

    useEffect

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { auth } from "../firebase";
 import ReactModal from "react-modal";
