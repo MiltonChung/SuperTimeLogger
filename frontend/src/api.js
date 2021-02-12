c0e12676b536e5744eb41312862cd4fa9733b395
Milton Chung <miltonjchung@gmail.com>
2021-02-11 21:55:53 -0800

    url fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,3 @@
-export const apiURL = "https://study-time-log.herokuapp.com/";
+export const apiURL = "https://study-time-log.herokuapp.com";
 // http://localhost:5000
 // https://study-time-log.herokuapp.com/
